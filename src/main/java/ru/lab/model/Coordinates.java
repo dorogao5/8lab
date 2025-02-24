8a35145bc07ec37fe7c3165b635222b89e2faed4
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2025-02-24 13:41:21 +0300

    base

--- src/main/java/ru/lab/model/Coordinates.java
+++ src/main/java/ru/lab/model/Coordinates.java
@@ -1,6 +1,48 @@
 package ru.lab.model;
 
+/**
+ * Класс, описывающий координаты.
+ * <p>
+ * Поля:
+ * <ul>
+ *   <li>x: значение типа long, максимальное значение 225;</li>
+ *   <li>y: целое число (Integer), не может быть null, максимальное значение 493;</li>
+ * </ul>
+ */
 public class Coordinates {
-    private long x; //Максимальное значение поля: 225
-    private Integer y; //Максимальное значение поля: 493, Поле не может быть null
+    private long x; // максимум 225
+    private Integer y; // не может быть null, максимум 493
+
+    /**
+     * Конструктор координат.
+     *
+     * @param x координата x (максимум 225)
+     * @param y координата y (не null, максимум 493)
+     */
+
+    public Coordinates(long x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+    public void setX(long x) {
+        this.x = x;
+    }
+    public Integer getY() {
+        return y;
+    }
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
