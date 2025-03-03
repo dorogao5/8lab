9864ccd8624395f8a1a4964c2b87fbcdbe3caa4e
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2025-03-03 12:49:01 +0300

    a little more

--- src/main/java/ru/lab/commands/ExecuteScript.java
+++ src/main/java/ru/lab/commands/ExecuteScript.java
@@ -1,4 +1,13 @@
 package ru.lab.commands;
 
-public class ExecuteScript extends Command{
+public class ExecuteScript implements Command {
+    @Override
+    public void execute(String[] args) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
 }
